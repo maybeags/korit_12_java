cb6f26577e25416ea9aa2f0f7d9d31e77ddf3592
maybeags <suren0709@gmail.com>
2025-12-22 17:00:50 +0900

    feat: operator lessoning

---
+++ src/main/java/ch02_operator/Operator02.java
@@ -0,0 +1,15 @@
+package ch02_operator;
+/*
+    논리 연산자
+    true(1) / false(0) : 컴퓨터가 읽을 수 있는 두 숫자
+    boolean : true / false를 구분하는 자료형
+
+    AND(&&) : 곱 연산자 T x T = T / T x F = F / F x F = F -> 하나라도 false면 false
+    OR(||) : 합 연산자 T + T = T / T + F = T / F + F = F -> 하나라면 true면 true
+    Not(!) : true 값을 false로, false 값을 true로 역전시키는 연산자
+ */
+public class Operator02 {
+    public static void main(String[] args) {
+
+    }
+}
