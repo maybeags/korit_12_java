946a05c6a244e813fe1f83b045c7e4870e6b4b9d
maybeags <suren0709@gmail.com>
2025-12-24 14:16:13 +0900

    feat: star wrting

---
+++ src/main/java/ch06_loops/Loop08Star3.java
@@ -0,0 +1,19 @@
+package ch06_loops;
+/*
+            *
+           **
+          ***
+         ****
+        *****
+
+       *****
+        ****
+         ***
+          **
+           *
+ */
+public class Loop08Star3 {
+    public static void main(String[] args) {
+
+    }
+}
