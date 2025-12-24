16b63a277a4a29fd297150748223c2dc3570fc43
maybeags <suren0709@gmail.com>
2025-12-24 16:14:42 +0900

    feat: method lesson #2

--- src/main/java/ch07_methods/Method01.java
+++ src/main/java/ch07_methods/Method01.java
@@ -31,7 +31,37 @@ public class Method01 {
         return score * 3;
     }
 
+    // 번외 -> 매개변수의 개수가 여러 개 일 수 있다.
+    public static String introduce(String name, int age) {
+        return "제 이름은 " + name + "이고, 나이는 " + age + "살입니다.\n내년에는 " + (age+1) + "살이 됩니다.";
+    }
+
     public static void main(String[] args) {
+        introduce("안근수", 38);
+        System.out.println(introduce("안근수", 38));
+/*
+    return이 있는 이유는 기본적으로 데이터의 조작(편집)을 위해서입니다. 그리고
+    특정 메서드의 결과값(return)이 다른 메서드의 argument로 사용될 수도 있습니다.
+
+    함수형 프로그래밍(functional programming) :
+        메서드1의 return 값이 메서드2의 argument가 되고, 메서드2의 return값이
+        메서드3의 argument가 되는 방식으로,
+
+        첫 번째 메서드로부터 마지막 메서드까지의 흐름을 통해 프로그램이 이어지는 방식
+
+        간단 예시
+ */
+        String name;
+        int age;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("이름을 입력하세요 >>> ");
+        name = scanner.nextLine();
+        System.out.print("나이를 입력하세요 >>> ");
+        age = scanner.nextInt();
+        System.out.println(introduce(name, age));
+
+
+
         // method 호출 영역
         call1();                // 얘는 argument가 없는거네요.
         call2("잠오고 피곤하고 공부가 하기 싫은데 굳이 선택하라고 하면 자습이 하고 싶다");
@@ -47,10 +77,10 @@ public class Method01 {
         System.out.println(call4(4.5));
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("숫자 입력하세요 >>> ");
-        int num = scanner.nextInt();
-        System.out.println(num);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("숫자 입력하세요 >>> ");
+//        int num = scanner.nextInt();
+//        System.out.println(num);
 
     }
 }
