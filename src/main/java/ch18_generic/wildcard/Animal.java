4bce32eef646dfc167ef2e4f70f8fa0102cf310d
maybeags <suren0709@gmail.com>
2026-01-12 17:49:48 +0900

    feat: 20260112 casting / singleton / generic

--- src/main/java/ch18_generic/wildcard/Animal.java
+++ src/main/java/ch18_generic/wildcard/Animal.java
@@ -1,4 +1,31 @@
 package ch18_generic.wildcard;
+/*
+     git add .
+     git commit -m "feat: 20260112 casting / singleton / generic"
+     git push
+     자리 정리
+     자격 증명 삭제
+     출결 통제 후 퇴실 체크
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
 public class Animal {
 }
