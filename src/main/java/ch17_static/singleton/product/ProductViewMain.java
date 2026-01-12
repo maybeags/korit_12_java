9075a91eb663f27e5cefe947e7ee34326f210f1d
maybeags <suren0709@gmail.com>
2026-01-12 09:11:05 +0900

    Feat: guidelines

--- src/main/java/ch17_static/singleton/product/ProductViewMain.java
+++ src/main/java/ch17_static/singleton/product/ProductViewMain.java
@@ -1,4 +1,10 @@
 package ch17_static.singleton.product;
-
+/*
+    git add .
+    git commit -m "feat : 20260109 casting / static / object / builder"
+    git push
+    자리 정리
+    자격 증명 삭제
+ */
 public class ProductViewMain {
 }
